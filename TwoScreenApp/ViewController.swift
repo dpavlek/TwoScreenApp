56e0c0806a210effa68366625fe2f09b22e4c3dc
COBE Osijek <daniel.pavlekovic@icloud.com>
2017-07-17 16:47:14 +0200

    Working App

--- TwoScreenApp/ViewController.swift
+++ TwoScreenApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var surnameLabel: UITextField!
+    
+    let descSegueIdentifier = "descriptionSegue"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+        if segue.identifier == descSegueIdentifier {
+            if let destinationViewController = segue.destination as? DescriptionViewController {
+                destinationViewController.name = nameLabel.text!
+                destinationViewController.surname = surnameLabel.text!
+            }
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            guard let firstName = nameLabel.text, !firstName.isEmpty else {
+                return false
+            }
+            guard let lastName = surnameLabel.text, !lastName.isEmpty else {
+                return false
+            }
+            return true
+    }
 }
-
